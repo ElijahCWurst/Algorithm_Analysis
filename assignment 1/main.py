634bc83e8f88739b11ba99d7d4ab1c0831c95214
ElijahWurst <elijahwurst@gmail.com>
2024-01-22 12:37:45 -0700

    HI

--- assignment 1/main.py
+++ assignment 1/main.py
@@ -1,14 +1,14 @@
 import math
 from scipy.special import lambertw
 
-formatNumber = lambda a : f"{a[0]}.{a[1:7]}*10^{len(a)-1}"
+formatNumber = lambda a : f"{a[0]}.{a[1:6]}*10^{len(a)-1}"
 logN = lambda a : ("2^" + str(a))
 rootN = lambda a : formatNumber(str(a**2))
 N = lambda a : formatNumber(str(a))
-nLogN = lambda a : formatNumber(str(int(math.e**lambertw(a*math.log(2, math.e)).real)))
-nSquared = lambda a : formatNumber(str(int(math.sqrt(a))))
-nCubed = lambda a : formatNumber(str(int(math.cbrt(a))))
-exponentialN = lambda a : formatNumber(str(int(math.log2(a))))
+nLogN = lambda a : str(int(math.e**lambertw(a*math.log(2, math.e)).real))
+nSquared = lambda a : str(int(math.sqrt(a)))
+nCubed = lambda a : str(int(math.cbrt(a)))
+exponentialN = lambda a : str(int(math.log2(a)))
 factorialN = lambda a : int(math.e**(lambertw(math.log(((a)/math.sqrt(2*math.pi)), math.e)/math.e).real+1)-0.5)
 
 microSeconds = [1000000, 60*1000000, 60*60*1000000, 24*60*60*1000000, 30*24*60*60*1000000, 365*24*60*60*1000000, 100*365*24*60*60*1000000]
