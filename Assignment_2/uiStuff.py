313372e2fd1df6c0bc74953a8c373da163061862
ElijahWurst <elijahwurst@gmail.com>
2024-01-26 15:21:27 -0700

    Messing with UI

---
+++ Assignment_2/uiStuff.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+
+# Add widgets and functionality here
+button = tk.Button(window, text="Click Me!")
+button.pack()
+
+
+# Start the main event loop
+window.mainloop()
---
+++ WPFapp/WPFapp/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace WPFapp
+{
+    public partial class App : Application
+	{
+	}
+}
