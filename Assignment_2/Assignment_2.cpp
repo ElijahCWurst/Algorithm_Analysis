b773b81b76b7ba1c0e90e13ea7ef0e26dd251462
ElijahWurst <elijahwurst@gmail.com>
2024-01-31 15:56:46 -0700

    bug fix and removed rouge project (with @TimothyZink)

--- Assignment_2/Assignment_2.cpp
+++ Assignment_2/Assignment_2.cpp
@@ -7,12 +7,18 @@
 
 using namespace std;
 
-void bubbleSort(int arr[], int n)
+const auto timeLimit = chrono::hours(2);
+
+short bubbleSort(int arr[], size_t n, chrono::high_resolution_clock::time_point time)
 {
-	int i, j;
+	size_t i, j;
 	bool swapped;
 	for (i = 0; i < n - 1; i++)
 	{
+		if (chrono::high_resolution_clock::now() - time > timeLimit)
+		{
+			return (short)((i * 100) / n);
+		}
 		swapped = false;
 		for (j = 0; j < n - i - 1; j++)
 		{
@@ -25,12 +31,18 @@ void bubbleSort(int arr[], int n)
 		if (swapped == false)
 			break;
 	}
+	return 100;
 }
-void insertionSort(int arr[], int n)
+short insertionSort(int arr[], size_t n, chrono::high_resolution_clock::time_point time)
 {
-	int i, key, j;
+	size_t i, j;
+	int key;
 	for (i = 1; i < n; i++)
 	{
+		if (chrono::high_resolution_clock::now() - time > timeLimit)
+		{
+			return (short)((i * 100) / n);
+		}
 		key = arr[i];
 		j = i - 1;
 		
@@ -42,12 +54,13 @@ void insertionSort(int arr[], int n)
 		}
 		arr[j + 1] = key;
 	}
+	return 100;
 }
-void merge(int arr[], int l, int m, int r)
+void merge(int arr[], size_t l, size_t m, size_t r)
 {
-	int i, j, k;
-	int n1 = m - l + 1; // size of left subarray
-	int n2 = r - m; // size of right subarray
+	size_t i, j, k;
+	size_t n1 = m - l + 1; // size of left subarray
+	size_t n2 = r - m; // size of right subarray
 	
 	//dynamically allocated temp arrays
 	int *L = new int[n1];
@@ -84,16 +97,16 @@ void merge(int arr[], int l, int m, int r)
 		arr[k++] = R[j++];
 	}
 }
-void mergeSort(int arr[], int l, int r)
+void mergeSort(int arr[], size_t l, size_t r)
 {
 	if (l >= r)
 		return;
-	int m = l + (r - l) / 2;
+	size_t m = l + (r - l) / 2;
 	mergeSort(arr, l, m);
 	mergeSort(arr, m + 1, r);
 	merge(arr, l, m, r);
 }
-void importFile(int arr[], int n, string fileName)
+void importFile(int arr[], size_t n, string fileName)
 {
 	ifstream file;
 
@@ -112,7 +125,7 @@ void importFile(int arr[], int n, string fileName)
 
 	file.close();
 }
-void printArray(int arr[], int n)
+void printArray(int arr[], size_t n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -120,20 +133,25 @@ void printArray(int arr[], int n)
 	}
 	cout << endl;
 }
-void mergeWrapper(int* arr, int size) {
+short mergeWrapper(int* arr, size_t size, chrono::high_resolution_clock::time_point time) {
+
 	mergeSort(arr, 0, size - 1);
+	return 100;
 }
-void timeFunction(void (*func)(int*, int), string name, int size, string fileName) {
+void timeFunction(short (*func)(int*, size_t, chrono::high_resolution_clock::time_point), string name, size_t size, string fileName) {
 	int* arr = new int[size];
 	importFile(arr, size, fileName);
 	auto start = chrono::high_resolution_clock::now();
-	func(arr, size);
+	short precentDone = func(arr, size, start);
 	auto stop = chrono::high_resolution_clock::now();
 	auto duration = chrono::duration_cast<chrono::nanoseconds>(stop - start);
 
-	cout << "\t" << left << setw(21) << name + ":"
-		<< left << setw(10) << duration.count() / 1000000000.0 << "seconds" << endl;
-	delete[] arr;
+	cout << "\t" << left << setw(21) << name + ":";
+	if (precentDone == 100)
+		cout << left << setw(10) << duration.count() / 1000000000.0 << "seconds" << endl;
+	else
+		cout << left << setw(10) << "Timed out (" << precentDone << "%)" << endl;
+	delete[] arr; // free memory and prevent memory leaks
 }
 
 int main()
@@ -142,27 +160,35 @@ int main()
 	const int TEN_M = 10000000;			//should be TEN_M
 	
 	const size_t FILECOUNT = 6;
-	string files[6] = {
-		"SortedRandomNumbersLarge.txt",
-		"RandomNumbersLarge.txt",
-		"ReversedRandomNumbersLarge.txt",
-		"SortedRandomNumbersSmall.txt",
-		"RandomNumbersSmall.txt",
-		"ReversedRandomNumbersSmall.txt"
+	struct File
+	{
+		string FileName;
+		int Size;
 	};
-	string names[3] = { "Merge Sort", "Insertion Sort", "Bubble Sort" };
-	size_t sizes[6] = { TEN_M, TEN_M, TEN_M, HUNDRED_K, HUNDRED_K, HUNDRED_K };
-	void (*sorts[3])(int*, int) = { mergeWrapper, insertionSort, bubbleSort };
-
-	//timeFunction(mergeWrapper, "Merge Sort", HUNDRED_M, "../Data/RandomNumbersLarge.txt");
-	//timeFunction(sorts[0], "Merge Sort", TEN_M, "../Data/RandomNumbersLarge.txt");
-
+	File files[6] = {
+		{ "SortedRandomNumbersSmall.txt", HUNDRED_K },
+		{ "RandomNumbersSmall.txt", HUNDRED_K },
+		{ "ReversedRandomNumbersSmall.txt", HUNDRED_K },
+		{ "SortedRandomNumbersLarge.txt", TEN_M },
+		{ "RandomNumbersLarge.txt", TEN_M },
+		{ "ReversedRandomNumbersLarge.txt", TEN_M }
+	};
+	struct Sorter
+	{
+		string Name;
+		short (*Function)(int*, size_t, chrono::high_resolution_clock::time_point);
+	};
+	Sorter sorts[3] = {
+		{ "Merge Sort", mergeWrapper },
+		{ "Insertion Sort", insertionSort },
+		{ "Bubble Sort", bubbleSort }
+	};
 	for (size_t i = 0; i < FILECOUNT; i++)
 	{
-		cout << files[i] << ": " << endl;
+		cout << files[i].FileName << ": " << endl;
 		for (size_t j = 0; j < 3; j++)
 		{
-			timeFunction(sorts[j], names[j], sizes[i], "../Data/" + files[i]);
+			timeFunction(sorts[j].Function, sorts[j].Name, files[i].Size, "../Data/" + files[i].FileName);
 		}
 	}
 }
--- WPFapp/WPFapp/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace WPFapp
-{
-    public partial class App : Application
-	{
-	}
-}
